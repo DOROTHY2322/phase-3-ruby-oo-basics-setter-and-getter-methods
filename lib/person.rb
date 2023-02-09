1c6dc2ffd331035df16a663dbcad527d5e0e7256
DOROTHY2322 <kimelidorothy@gmail.com>
2023-02-09 18:57:24 +0300

    setter and getter methods

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,21 @@
 class Person
-
+    #setter
+def name=(name)
+    @name = name
 end
+#getter
+def name
+    @name
+end
+end
+
+class Person
+    #setter
+    def job=(job)
+        @job = job
+        end
+        #getter
+        def job
+            @job
+            end
+            end
